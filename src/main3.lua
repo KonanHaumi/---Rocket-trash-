381634219b29c1a1819d83e811333798dcce3c41
KonanHaumi <krivovaav@mail.ru>
2023-09-11 16:41:34 +0400

    first
    добавила свой файл flightProcess-10, main пока ссылается на мой файл

---
+++ src/main3.lua
@@ -0,0 +1,26 @@
+-- $Name: Самая интересная игра!$
+-- $Version: 0.5$
+-- $Author: Анонимный любитель текстовых приключений$
+-- $Info: Это ремейк старой игры\nС ZX specturm$
+
+require "fmt" -- некоторые функции форматирования
+fmt.para = true -- включить режим параграфов (отступы)
+
+game.act = 'Не работает.';
+game.use = 'Это не поможет.';
+game.inv = 'Зачем мне это?';
+
+room {
+	nam = 'main';
+	disp = "Начало игры";
+	dsc = [[тут что-то про начало игры]];
+	way = {path {'Начать', "Полет"} };
+}
+
+include "launchpad"
+include "ship"
+include "flight"
+include "landing"
+include "flightProcess-10"
+
+
